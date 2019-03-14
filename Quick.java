8964b8783987998ddf725c2e6cc975fa7676eaf2
Calvin Chu <cchu00@stuy.edu>
2019-03-13 23:02:32 -0400

    changed driver and modified quickselect and partition

--- Quick.java
+++ Quick.java
@@ -7,12 +7,15 @@ public class Quick {
    int start = 0, end = data.length-1, pivot = partition(data, start, end);  //sets up initial values to run partition
 	 while (k != pivot) { //while k is not = to pivot index, run the partition method, update the start/end
       pivot = partition(data, start, end);
-			if (k < pivot) end = pivot;
-			else start = pivot;
+			if (k < pivot) end = pivot - 1;
+			else start = pivot + 1;
 	 }
    return data[k]; //returns k when = to pivot index
  }
  public static int partition (int [] data, int start, int end){
+    if (start == end) return start;
+    Random r = new Random();
+    int halfpass = 0;
     int median = 0;
     if ((data[start] <= data[end] && data[start] >= data[start + (end - start)/2]) || (data[start] >= data[end] && data[start] <= data[start + (end - start)/2])) median = start;
     if ((data[end] <= data[start] && data[end] >= data[start + (end - start)/2]) || (data[end] >= data[start] && data[end] <= data[start + (end - start)/2])) median = end;
@@ -24,10 +27,10 @@ public class Quick {
     pivotIndex = start;
     while (end > start) { //loops through "current" data set to reorganize the values based on being > or < than pivot
       if (data[start] > pivot) {
-        swap(data, start, end);
-        end--;
-      }
-      else start++;
+          swap(data, start, end);
+          end--;
+        }
+      else {start++;}
     }
     // returns the pivot index based on pivot and start
       if (data[start] < pivot) {
